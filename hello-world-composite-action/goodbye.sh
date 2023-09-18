f9ac9f60affd4f2d2c9dab19fb0dfbcc4ad41a10
unknown <adityashewale1994@gmail.com>
2023-09-18 10:18:27 +0530

    Testing env variables using shell

--- hello-world-composite-action/goodbye.sh
+++ hello-world-composite-action/goodbye.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 export CONCURRENCY="$(nproc)"
+export MAKEFLAGS="-j \${CONCURRENCY}"
+export BEEGFS_VERSION="\$(test -n "\${CI_COMMIT_TAG}" && echo "\${CI_COMMIT_TAG}" || git describe --tags --match '*.*' --abbrev=8)"
+export GPG_TTY=\$(tty)
 echo "$CONCURRENCY"
-echo "good bye"
-echo "$a"
+echo "$MAKEFLAGS"
+echo "$BEEGFS_VERSION"
+
